c0b0ca5f6ca1ca8be53f2f3a92936babf1785d70
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-13 16:53:47 +0700

    add some borders

--- road.js
+++ road.js
@@ -10,6 +10,15 @@ class Road {
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
 
   getLaneCenter(laneIndex) {
