0ae2b925fc58493628aa8d0cb1a947e7c9958ea8
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-13 13:29:23 +0700

    create car physics to move like a car

--- car.js
+++ car.js
@@ -7,6 +7,10 @@ class Car {
 
     this.speed = 0;
     this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
+    this.rotateSpeed = 0.03;
 
     this.controls = new Controls();
   }
@@ -18,17 +22,47 @@ class Car {
     if (this.controls.reverse) {
       this.speed -= this.acceleration;
     }
-    this.y -= this.speed;
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += this.rotateSpeed * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= this.rotateSpeed * flip;
+      }
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
