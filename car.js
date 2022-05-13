4c75c10031df434055b78ebe4f89488de85e7c75
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-13 13:15:04 +0700

    implement canvas, car, moving up down

---
+++ car.js
@@ -0,0 +1,34 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.acceleration = 0.2;
+
+    this.controls = new Controls();
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+    this.y -= this.speed;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
+}
